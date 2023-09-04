31600aa3239a9dd9596f31ef685f95ec6efe5229
Harshit Kant <96339258+Harshitkant21@users.noreply.github.com>
2023-09-04 22:58:28 +0530

    equilibriumPoint.java

---
+++ equilibriumPoint.java
@@ -0,0 +1,29 @@
+// Platform GFG https://practice.geeksforgeeks.org/problems/equilibrium-point-1587115620/1?page=1&sprint=a663236c31453b969852f9ea22507634&sortBy=submissions
+
+class Solution {
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long arr[], int n) {
+
+        // Your code here
+        long total=0;
+        long left=0;
+        for(int i=0;i<n;i++){
+            total+=arr[i];
+        }
+        long right=total;
+        for(int i=0;i<n;i++){
+            right-=arr[i];
+            if(left==right){
+                return i+1;
+            }
+            left+=arr[i];
+        }
+        return -1;
+        
+    }
+}
+
+//Time Complexity O(n)
+//Space Complexity O(1)
