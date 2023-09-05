56cb8383519a8b39b3c4ab3c80e3d0fd693577eb
Harshit Kant <96339258+Harshitkant21@users.noreply.github.com>
2023-09-05 23:12:43 +0530

    Create thirdMax.java
    Day #2 of 100_Days_of_Code 
    
    Problem Statement :
    
    Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.

---
+++ thirdMax.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        // Initialize three variables to store the top three maximum values.
+        Integer curr = null;  // The current maximum value
+        Integer sec = null;   // The second maximum value
+        Integer thir = null;  // The third maximum value
+        
+        // Iterate through the input array 'nums'
+        for (Integer i : nums) {
+            // Check if 'i' is equal to any of the current top three values, and skip it.
+            if (i.equals(curr) || i.equals(sec) || i.equals(thir)) {
+                continue;
+            }
+            
+            // Update the top three values based on the current element 'i'.
+            if (curr == null || i > curr) {
+                thir = sec;
+                sec = curr;
+                curr = i;
+            } else if (sec == null || i > sec) {
+                thir = sec;
+                sec = i;
+            } else if (thir == null || i > thir) {
+                thir = i;
+            }
+        }
+        
+        // Check if there is a valid third maximum value, and return it.
+        if (thir != null) {
+            return thir;
+        } else {
+            // If there is no third maximum, return the current maximum value.
+            return curr;
+        }
+    }
+}
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
