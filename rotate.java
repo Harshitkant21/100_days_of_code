d21538685d43f2b3ff31dad47b1da75aca99311a
Harshit Kant <96339258+Harshitkant21@users.noreply.github.com>
2023-10-02 22:16:32 +0530

    Update rotate.java

--- rotate.java
+++ rotate.java
@@ -1,7 +1,8 @@
 // Platform GFG https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1
+// Platform Leetcode https://leetcode.com/problems/rotate-array/description/?envType=study-plan-v2&envId=top-interview-150
 // Day #8 of 100 Days_Of_code
 
-
+// GFG solution
 class Compute {
     
     public void rotate(int arr[], int n) {
@@ -15,6 +16,46 @@ class Compute {
         
         // Place the temporarily stored last element at the beginning of the array
         arr[0] = temp;
+    }
+}
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+// Leetcode solution
+
+class Solution {
+    // Method to rotate an array to the right by k positions
+    public void rotate(int[] nums, int k) {
+        // Get the length of the array
+        int n = nums.length;
+        
+        // Ensure k is within a valid range by taking its modulo n
+        k = k % n;
+        
+        // Reverse the entire array
+        reverse(nums, 0, n - 1);
+        
+        // Reverse the first k elements
+        reverse(nums, 0, k - 1);
+        
+        // Reverse the remaining elements
+        reverse(nums, k, n - 1);
+    }
+    
+    // Helper method to reverse an array or a portion of it
+    public static void reverse(int[] arr, int start, int end) {
+        // Use two pointers, one starting from the beginning and the other from the end
+        while (start < end) {
+            // Swap the elements at the start and end positions
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            
+            // Move the pointers towards each other
+            start++;
+            end--;
+        }
     }
 }
 
